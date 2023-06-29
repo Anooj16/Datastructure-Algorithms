e0a0b0ce52297a538c2fd05edac88f16d031261e
Anooj <anoojappu64@gmail.com>
2023-06-28 20:45:36 -0700

    third commit

--- DataStructure& Algorithm/data structure workouts/array/interview.js
+++ DataStructure& Algorithm/data structure workouts/array/interview.js
@@ -58,4 +58,104 @@ const findSmallestMissingPositive = (nums) => {
     // 'left' or 'right' represents the peak element index
     return left;
   };
+
   
+  //3 find the majority element in an array (an element that appears more than n/2 times)?
+
+  function findMajorityElement(arr) {
+    const countMap = {}; // Hash map to store element counts
+  
+    for (let i = 0; i < arr.length; i++) {
+      const element = arr[i];
+      countMap[element] = (countMap[element] || 0) + 1; // Increment count for each element
+      if (countMap[element] > arr.length / 2) {
+        return element; // Found majority element
+      }
+    }
+  
+    return -1; // No majority element found
+  }
+  
+  // Example usage
+  const array = [2, 4, 3, 4, 4, 2, 4, 4, 6];
+  const majorityElement = findMajorityElement(array);
+  console.log("Majority element:", majorityElement);
+  
+
+  //4 find the median of two sorted arrays?
+
+  function findMedianSortedArrays(nums1, nums2) {
+    const merged = [];
+    let i = 0;
+    let j = 0;
+  
+    while (i < nums1.length && j < nums2.length) {
+      if (nums1[i] <= nums2[j]) {
+        merged.push(nums1[i]);
+        i++;
+      } else {
+        merged.push(nums2[j]);
+        j++;
+      }
+    }
+  
+    // Add the remaining elements from nums1
+    while (i < nums1.length) {
+      merged.push(nums1[i]);
+      i++;
+    }
+  
+    // Add the remaining elements from nums2
+    while (j < nums2.length) {
+      merged.push(nums2[j]);
+      j++;
+    }
+  
+    const middle = Math.floor(merged.length / 2);
+  
+    if (merged.length % 2 === 0) {
+      // If the merged array has an even length, return the average of the middle two elements
+      return (merged[middle - 1] + merged[middle]) / 2;
+    } else {
+      // If the merged array has an odd length, return the middle element
+      return merged[middle];
+    }
+  }
+  
+  // Example usage
+  const nums1 = [1, 3];
+  const nums2 = [2, 4, 5];
+  const median = findMedianSortedArrays(nums1, nums2);
+  console.log("Median:", median);
+
+  //5 sort an array of 0s, 1s, and 2s (also known as the Dutch National Flag problem)?
+
+      //To sort an array of 0s, 1s, and 2s (also known as the Dutch National Flag problem), you can use the "Dutch partitioning" algorithm. It involves maintaining three pointers to divide the array into three regions: the low region for 0s, the mid region for 1s, and the high region for 2s. Here's an example implementation in JavaScript:
+
+      function sortColors(nums) {
+        let low = 0;        // Pointer for the low region (0s)
+        let mid = 0;        // Pointer for the mid region (1s)
+        let high = nums.length - 1;   // Pointer for the high region (2s)
+      
+        while (mid <= high) {
+          if (nums[mid] === 0) {
+            // Swap the current element with the element at the low pointer
+            [nums[low], nums[mid]] = [nums[mid], nums[low]];
+            low++;
+            mid++;
+          } else if (nums[mid] === 1) {
+            // Move the mid pointer forward
+            mid++;
+          } else if (nums[mid] === 2) {
+            // Swap the current element with the element at the high pointer
+            [nums[mid], nums[high]] = [nums[high], nums[mid]];
+            high--;
+          }
+        }
+      }
+      
+      // Example usage
+      const nums = [2, 0, 1, 2, 1, 0];
+      sortColors(nums);
+      console.log("Sorted Array:", nums);
+      
--- DataStructure& Algorithm/data structure workouts/linkedList/interview.js
+++ DataStructure& Algorithm/data structure workouts/linkedList/interview.js
@@ -107,5 +107,145 @@ class ListNode {
     return false;
   };
   
+  //3 determine if two linked lists intersect? If they do, find the intersection point.
+
+  // Node class for a linked list
+class ListNode {
+  constructor(value) {
+    this.val = value;
+    this.next = null;
+  }
+}
+
+// Function to determine if two linked lists intersect and find the intersection point
+function getIntersectionNode(headA, headB) {
+  // Get the lengths of both linked lists
+  let lengthA = getLinkedListLength(headA);
+  let lengthB = getLinkedListLength(headB);
+
+  // Calculate the length difference between the two linked lists
+  let lengthDiff = Math.abs(lengthA - lengthB);
+
+  // Move the head of the longer list by the length difference
+  if (lengthA > lengthB) {
+    while (lengthDiff > 0) {
+      headA = headA.next;
+      lengthDiff--;
+    }
+  } else {
+    while (lengthDiff > 0) {
+      headB = headB.next;
+      lengthDiff--;
+    }
+  }
+
+  // Traverse both linked lists in parallel until they intersect or reach the end
+  while (headA !== null && headB !== null) {
+    if (headA === headB) {
+      return headA; // Intersection point found
+    }
+    headA = headA.next;
+    headB = headB.next;
+  }
+
+  return null; // No intersection point found
+}
+
+// Function to get the length of a linked list
+function getLinkedListLength(head) {
+  let length = 0;
+  let current = head;
+  while (current !== null) {
+    length++;
+    current = current.next;
+  }
+  return length;
+}
+
+// Example usage
+
+// Linked list A: 1 -> 2 -> 3 -> 4
+const headA = new ListNode(1);
+const nodeA2 = new ListNode(2);
+const nodeA3 = new ListNode(3);
+const nodeA4 = new ListNode(4);
+headA.next = nodeA2;
+nodeA2.next = nodeA3;
+nodeA3.next = nodeA4;
+
+// Linked list B: 6 -> 7 -> 8 -> 9 -> 3 -> 4
+const headB = new ListNode(6);
+const nodeB2 = new ListNode(7);
+const nodeB3 = new ListNode(8);
+const nodeB4 = new ListNode(9);
+headB.next = nodeB2;
+nodeB2.next = nodeB3;
+nodeB3.next = nodeB4;
+nodeB4.next = nodeA3;
+// The two linked lists intersect at nodeA3
+
+const intersectionNode = getIntersectionNode(headA, headB);
+if (intersectionNode !== null) {
+  console.log("Intersection Point:", intersectionNode.val);
+} else {
+  console.log("No Intersection Point Found");
+}
+
+
+//4 find the nth node from the end of a linked list
+
+    // Node class for a linked list
+class ListNode {
+  constructor(value) {
+    this.val = value;
+    this.next = null;
+  }
+}
+
+// Function to find the nth node from the end of a linked list
+function findNthFromEnd(head, n) {
+  let slow = head;
+  let fast = head;
+
+  // Move the fast pointer n nodes ahead of the slow pointer
+  for (let i = 0; i < n; i++) {
+    if (fast === null) {
+      return null; // Linked list length is less than n
+    }
+    fast = fast.next;
+  }
+
+  // Move both pointers until the fast pointer reaches the end of the list
+  while (fast !== null) {
+    slow = slow.next;
+    fast = fast.next;
+  }
+
+  return slow;
+}
+
+// Example usage
+
+// Linked list: 1 -> 2 -> 3 -> 4 -> 5 -> 6
+const head = new ListNode(1);
+const node2 = new ListNode(2);
+const node3 = new ListNode(3);
+const node4 = new ListNode(4);
+const node5 = new ListNode(5);
+const node6 = new ListNode(6);
+head.next = node2;
+node2.next = node3;
+node3.next = node4;
+node4.next = node5;
+node5.next = node6;
+
+const n = 3;
+const nthNodeFromEnd = findNthFromEnd(head, n);
+if (nthNodeFromEnd !== null) {
+  console.log(`The ${n}th node from the end is ${nthNodeFromEnd.val}`);
+} else {
+  console.log(`The linked list has less than ${n} nodes`);
+}
+
 
   
